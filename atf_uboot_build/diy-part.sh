646aa95f57e70975ded63f5ddc378c4242cd42db
lgs2007m <116912542@qq.com>
2023-01-19 21:53:35 +0800

    Update diy-part.sh

--- atf_uboot_build/diy-part.sh
+++ atf_uboot_build/diy-part.sh
@@ -11,6 +11,6 @@ sed -i 's/112640k(ubi)/501760k(ubi)/' uboot-mtk-20220606/configs/mt7986_redmi_ax
 ## ----------------------------------------
 
 ## 修改nandinfo为4k+256，这个可有可无，不影响
-sed -i 's/nandinfo=2k+64/nandinfo=4k+256/' uboot-mtk-20220606/arch/arm/mach-mediatek/Kconfig
+#sed -i 's/nandinfo=2k+64/nandinfo=4k+256/' uboot-mtk-20220606/arch/arm/mach-mediatek/Kconfig
 ## 修改spi-nand的spi-max-frequency，原闪存104MHz对应52000000，新闪存TC58CVG2S0HRAIJ闪存最大频率133MHz修改为66500000
 sed -i 's/spi-max-frequency = <52000000>/spi-max-frequency = <66500000>/' uboot-mtk-20220606/arch/arm/dts/mt7986a-redmi-ax6000.dts
