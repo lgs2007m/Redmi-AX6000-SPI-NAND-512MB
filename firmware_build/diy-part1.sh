3ca4c7b136307f8ed43d32fed31e96abf09fc0f7
lgs2007m <116912542@qq.com>
2023-01-21 21:23:27 +0800

    Update diy-part1.sh

--- firmware_build/diy-part1.sh
+++ firmware_build/diy-part1.sh
@@ -18,6 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 
+## 修改image/mt7986.mk中的BLOCKSIZE := 128k为256k
+sed -i '/mt7986a-xiaomi-redmi-router-ax6000-uboot/{n;n;n;n;s/128k/256k/;}' target/linux/mediatek/image/mt7986.mk
+## 修改image/mt7986.mk中的PAGESIZE := 2048为4096
+sed -i '/mt7986a-xiaomi-redmi-router-ax6000-uboot/{n;n;n;n;n;s/2048/4096/;}' target/linux/mediatek/image/mt7986.mk
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
