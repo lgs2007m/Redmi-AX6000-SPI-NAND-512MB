039e73ce68255f530ec1359e80929c9991a8cdca
lgs2007m <116912542@qq.com>
2023-01-23 10:08:23 +0800

    Update diy-part1.sh

--- firmware_build/diy-part1.sh
+++ firmware_build/diy-part1.sh
@@ -18,14 +18,13 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 
-## 修改image/mt7986.mk中的BLOCKSIZE := 128k为256k
+## 修改image/mt7986.mk中的BLOCKSIZE := 128k为256k，PAGESIZE := 2048为4096，好像可有可无
 sed -i '/mt7986a-xiaomi-redmi-router-ax6000-uboot/{n;n;n;n;s/128k/256k/;}' target/linux/mediatek/image/mt7986.mk
-## 修改image/mt7986.mk中的PAGESIZE := 2048为4096
 sed -i '/mt7986a-xiaomi-redmi-router-ax6000-uboot/{n;n;n;n;n;s/2048/4096/;}' target/linux/mediatek/image/mt7986.mk
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
-## 修改DTS的memory为512MB的0x40000000
+## 修改DTS的memory为512MB的0x20000000
 sed -i 's/reg = <0 0x40000000 0 0x10000000>/reg = <0 0x40000000 0 0x20000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
 ## 修改DTS的spi_nand的spi-max-frequency为52MHz，52000000
 sed -i 's/spi-max-frequency = <20000000>/spi-max-frequency = <52000000>/' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000-uboot.dts
